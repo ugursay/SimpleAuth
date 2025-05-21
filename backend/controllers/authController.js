d765a98b53f8ae2ead0f8428ae74fb219468f2b5
Uğur <ugursay3211@gmail.com>
2025-05-21 14:49:43 +0300

    SimpleAuth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import db from "../db.js";
+import { sendVerificaitionEmail } from "../utils/sendVerificaitionEmail.js";
 
 export const registerUser = async (req, res) => {
   const { username, email, password } = req.body;
@@ -17,11 +18,27 @@ export const registerUser = async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await db.execute(
+    const [result] = await db.execute(
       "INSERT INTO users (username, email, password) VALUES (?,?,?)",
       [username, email, hashedPassword]
     );
-    res.status(201).json({ message: "Kullanıcı başarıyla oluşturuldu." });
+
+    const userId = result.insertId;
+
+    const verificationToken = jwt.sign(
+      { id: userId, email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    await sendVerificaitionEmail(email, verificationToken);
+
+    res.status(201).json({
+      message:
+        "Kullanıcı başarıyla oluşturuldu. Lütfen E-mail'inizi doğrulayın.",
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "sunucu hatası." });
@@ -44,6 +61,12 @@ export const loginUser = async (req, res) => {
 
     const user = rows[0];
 
+    if (!user.isVerified) {
+      return res
+        .status(403)
+        .json({ message: "lütfen önce emalinizi doğrulayın" });
+    }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(401).json({ message: "şifre yanlış" });
@@ -56,5 +79,44 @@ export const loginUser = async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Sunucu hatası." });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { token } = req.params;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const email = decoded.email;
+
+    const [rows] = await db.execute(
+      "SELECT isVerified FROM users WHERE email=?",
+      [email]
+    );
+
+    if (rows.length === 0) {
+      return res.status(400).json({ message: "Email bulunamadı." });
+    }
+
+    if (rows[0].isVerified) {
+      return res.status(400).json({ message: "Email zaten doğrulanmış." });
+    }
+
+    await db.execute("UPDATE users SET isVerified = TRUE WHERE email=?", [
+      email,
+    ]);
+
+    res.send(`
+      <script>
+        alert("Email başarıyla doğrulandı.");
+        window.location.href = "http://localhost:3000/login";
+      </script>
+    `);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({
+      message: "geçersiz veya süresi dolmuş token",
+      error: err.message,
+    });
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// app.use("/api", authRoutes);
 app.use("/api", authRoutes);
 
 app.listen(5000, () => {
